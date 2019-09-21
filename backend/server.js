31d2be538474cee4d694504d602f864cbcefb440
liming wu <limwuly@gmail.com>
2019-09-20 20:08:21 -0400

    create 1st communitcation between backend/front

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,16 @@
 var express=require('express')
+var cors=require('cors')
 var app=express()
 
-app.get('/',(req,res)=>{
-    res.send('hello world')
+var post=[
+    {message:'hello'},
+    {message:'hiworld'}
+]
+
+app.use(cors())
+
+app.get('/posts',(req,res)=>{
+    res.send(post)
 })
 
 app.listen(3000)
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './api.service';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my frontend';
+  constructor(private apiService:ApiService){}
+
+  ngOnInit(){
+    this.apiService.getMessages();
+  }
 }
